66eda0aa0e58bf937762805a8e24388a4fa6816b
Abdul Rehman <abdulrehmansafdar2928@gmail.com>
2024-12-15 14:16:58 +0500

    Add missing functions and constants for internal node handling; refactor print functions for clarity

--- src/btree.c
+++ src/btree.c
@@ -4,6 +4,15 @@
 #include <stdint.h>
 #include <string.h>
 
+// Declare missing functions
+void internal_node_split_and_insert(Table *table, uint32_t parent_page_num, uint32_t child_page_num);
+void create_new_root(Table *table, uint32_t right_child_page_num);
+void indent(uint32_t level);
+
+// Define missing constants
+#define INTERNAL_NODE_MAX_CELLS 3
+#define INVALID_PAGE_NUM 0xFFFFFFFF
+
 /*** Leaf Node start ***/
 uint32_t *leaf_node_num_cells(void *node)
 {
@@ -344,38 +353,6 @@ void create_new_root(Table *table, uint32_t right_child_page_num)
   *node_parent(left_child) = table->root_page_num;
   *node_parent(right_child) = table->root_page_num;
 }
-
-// void print_tree(Pager* pager, uint32_t page_num, uint32_t indentation_level) {
-//   void* node = get_page(pager, page_num);
-//   uint32_t num_keys, child;
-//   switch (get_node_type(node)) {
-//     case NODE_LEAF:
-//       num_keys = *leaf_node_num_cells(node);
-//       indent(indentation_level);
-//       printf("- leaf (size %d)\n", num_keys);
-//       for (uint32_t i = 0; i < num_keys; i++) {
-//         indent(indentation_level + 1);
-//         printf("- %d\n", *leaf_node_key(node, i));
-//       }
-//       break;
-//     case NODE_INTERNAL:
-//       num_keys = *internal_node_num_keys(node);
-//       indent(indentation_level);
-//       printf("- internal (size %d)\n", num_keys);
-//       if (num_keys > 0) {
-//         for (uint32_t i = 0; i < num_keys; i++) {
-//           child = *internal_node_child(node, i);
-//           print_tree(pager, child, indentation_level + 1);
-
-//           indent(indentation_level + 1);
-//           printf("- key %d\n", *internal_node_key(node, i));
-//         }
-//         child = *internal_node_right_child(node);
-//         print_tree(pager, child, indentation_level + 1);
-//       }
-//       break;
-//   }
-// }
 
 // Add this non-recursive print function using stack
 void print_tree_iterative(Pager *pager, uint32_t root_page_num)
